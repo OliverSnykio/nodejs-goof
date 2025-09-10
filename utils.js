05a61c684a184399e4b195d9e5fccb24330c7d05
OliverSnykio <96267312+OliverSnykio@users.noreply.github.com>
2025-09-10 20:48:58 +0100

    Add Express server for customer onboarding

--- utils.js
+++ utils.js
@@ -26,3 +26,17 @@ module.exports = {
     res.end( body );
   }
 };
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/customerOnboarding', (req, res) => {
+  const name = req.query.name;
+  const uppercaseName = eval('"' + name + '"' + '.toUpperCase()');
+  res.send('Hi there, ' + uppercaseName);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
